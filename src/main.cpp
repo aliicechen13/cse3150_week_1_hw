629d01de0d2fdc8c69584074d32e953f94ed8c1d
Alice <aliicechen13@gmail.com>
2025-09-02 15:22:45 -0400

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ int main() {
 	int a = 3;
 	int b = 4;
 
-	cout << a << " + " << b << " = " << MathUtils::add(a, b) << endl;
-	cout << a << " * " << b << " = " << MathUtils::multiply(a, b) << endl;
-	cout << "square(" << a << ") = " << AdvancedMath::square(a) << endl;	
+	cout << "a + b = " << MathUtils::add(a, b) << endl;
+	cout << "a * b = " << MathUtils::multiply(a, b) << endl;
+	cout << "square(a) = " << AdvancedMath::square(a) << endl;	
 	return 0;
 }
